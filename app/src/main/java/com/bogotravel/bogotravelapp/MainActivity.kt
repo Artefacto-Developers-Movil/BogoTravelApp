aa5b73800f754019ba6224e901568336736a4a08
Victor Manuel <victormanuelestradavarela@Victors-MacBook-Air.local>
2021-11-12 21:17:01 -0500

    Interfaz parque victor

--- app/src/main/java/com/bogotravel/bogotravelapp/MainActivity.kt
+++ app/src/main/java/com/bogotravel/bogotravelapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.parque_metropolitano_simon_bolivar)
+        //setContentView(R.layout.activity_main)
     }
 }
